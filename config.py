05e6786c602221b75a5b50ba1374b7c66136534b
JasonWong0829 <130625124+JasonWong0829@users.noreply.github.com>
2023-05-01 22:46:01 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ customhost = "employee.cozsdhyiskuj.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "123456789"
 customdb = "employee"
-custombucket = "wongjetsheng-employee"
+custombucket = "wongjetsheng-epmloyee"
 customregion = "us-east-1"
 
