4f78a9cb2110d3408351e4ab668ee795520b9dd8
JasonWong0829 <130625124+JasonWong0829@users.noreply.github.com>
2023-05-01 22:33:13 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cozsdhyiskuj.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "123456789"
+customdb = "employee"
+custombucket = "wongjetsheng-employee"
+customregion = "us-east-1"
+
